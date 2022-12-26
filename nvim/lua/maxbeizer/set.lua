2bcda21fd13360422dd5140973e0038205edae04
Max Beizer <max.beizer@gmail.com>
2022-12-26 14:07:50 -0500

    Update set

--- nvim/lua/maxbeizer/set.lua
+++ nvim/lua/maxbeizer/set.lua
@@ -7,8 +7,10 @@ vim.opt.joinspaces = false
 vim.opt.list = true
 vim.opt.listchars = { trail = "~", nbsp = "~", tab = "| " }
 vim.opt.number = true
-vim.opt.numberwidth = 5
+vim.opt.numberwidth = 4
+vim.opt.scrolloff = 8
 vim.opt.shiftwidth = 2
+vim.opt.sidescrolloff = 8
 vim.opt.smartcase = true
 vim.opt.tabstop = 2
 vim.opt.textwidth = 80
